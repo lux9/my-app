0a1c959ea8d49b4fb0866067d10ec50e10c5a6e2
lux <lux>
2019-11-17 18:23:48 -0300

    Fix skills and assigned equip files

--- db/migrate/20191117180956_create_jobs.rb
+++ db/migrate/20191117180956_create_jobs.rb
@@ -2,7 +2,6 @@ class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
       t.string :role
-      t.references :employee, null: false, foreign_key: true
       t.references :office, null: false, foreign_key: true
       t.string :type
 
